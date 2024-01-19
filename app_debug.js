002dd2524f8e3ce0853fda2cc4df6aa1cb72f7c9
Toraalv <toralv_p@proton.me>
2024-01-19 15:05:34 +0000

    fixed new certificates and ogp tags

--- app_debug.js
+++ app_debug.js
@@ -1,46 +1,19 @@
 const express = require("express");
 const app = express();
-const port = 1770;
+const port = 17720;
 const path = require("path");
 const fs = require("fs");
 
 const homeStatic = express.static(path.join(__dirname, "public"));
 const rtkStatic = express.static(path.join(__dirname, "rtk-joyo-comparer/Server/public"));
-const utasukiStatic = express.static(path.join(__dirname, "UtaSuki/public"));
 
-// serving two different statics based on where you are on the site
-// feels stupid, but I don't want to hardcode inside of rtk-joyo-comparer
-// since the request in there refers to this local public directory
-// app.use("/static", ( req, res, next) => {
-// 	if (req.header("Referer").slice(-17) == "rtk-joyo-comparer") {
-// 		rtkStatic(req, res, next);
-// 	} else {
-// 		homeStatic(req, res, next);
-// 	}
-// });
+// やれやれだぜ
 app.use("/static", ( req, res, next) => {
-	// console.log(req.url);
-	// console.log(req.header("Referer").match("UtaSuki")[0]);
-	if (req.header("Referer").slice(-17) == "rtk-joyo-comparer") {
-		rtkStatic(req, res, next);
-	} else {
-		homeStatic(req, res, next);
-	}
-	// try { bara lös
-	// 	if (req.header("Referer").match("UtaSuki")[0] == "UtaSuki") {
-	// 		console.log("UTASUKI SOM FAN");
-	// 		utasukiStatic(req, res, next);
-	// 	} else {
-	// 		homeStatic(req, res, next);
-	// 	}
-	// } catch(error) {
-	// 	console.log("Error in your mum. I don't care");
-	// 	homeStatic(req, res, next);
-	// }
-	
+	if (req.header("Referer") != undefined)
+		if (req.header("Referer").slice(-17) == "rtk-joyo-comparer")
+			rtkStatic(req, res, next);
+	else homeStatic(req, res, next); // bruv
 });
-
-
 
 // for main site
 app.get("/", (req, res) => {
@@ -51,20 +24,6 @@ app.get("/", (req, res) => {
 // for rtk-joyo-comparer site
 app.get("/rtk-joyo-comparer", (req, res) => {
 	res.sendFile(__dirname + "/rtk-joyo-comparer/Server/index.html");
-});
-
-
-// for UtaSuki site
-app.get("/UtaSuki/", (req, res) => {
-	res.sendFile(__dirname + "/UtaSuki/index.html");
-});
-// this is not optimal
-app.get("/UtaSuki/add_item", (req, res) => {
-	console.log("dirname: " + __dirname);
-	res.sendFile(__dirname + "/UtaSuki/add_item.html"); 
-});
-app.get("/UtaSuki/year=*", (req, res) => {
-	res.sendFile(__dirname + "/UtaSuki/base_year.html");
 });
 
 
--- app_live.js
+++ app_live.js
@@ -18,15 +18,12 @@ const credentials = {
 const homeStatic = express.static(path.join(__dirname, "public"));
 const rtkStatic = express.static(path.join(__dirname, "rtk-joyo-comparer/Server/public"));
 
-// Serving two different statics based on where you are on the site
-// Feels stupid, but I don't want to hardcode inside of rtk-joyo-comparer
-// since the request in there refers to this local public directory
+// やれやれだぜ
 app.use("/static", ( req, res, next) => {
-	if (req.header("Referer").slice(-17) == "rtk-joyo-comparer") {
-		rtkStatic(req, res, next);
-	} else {
-		homeStatic(req, res, next);
-	}
+	if (req.header("Referer") != undefined)
+		if (req.header("Referer").slice(-17) == "rtk-joyo-comparer")
+			rtkStatic(req, res, next);
+	else homeStatic(req, res, next); // bruv
 });
 
 app.get("/", (req, res) =>
